04fa77b87b5b13c3c55235f4da7e5800655310bb
Jake <jakechalmers86@gmail.com>
2025-06-28 14:05:45 +0800

    Added all required product fields for database integration

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -12,5 +12,15 @@ namespace Core.Entities
         public required string Name { get; set; }
 
         public required string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public required string PictureUrl { get; set; }
+
+        public required string Type { get; set; }
+
+        public required string Brand { get; set; }
+
+        public int QuantitiyInStock { get; set; }
     }
 }
