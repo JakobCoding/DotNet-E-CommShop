b6ebad8ae98f55d0cf7a21453961418242b5b207
Jake <jakechalmers86@gmail.com>
2025-06-28 14:01:03 +0800

    Add required Name and Description properties to Product entity

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -8,5 +8,9 @@ namespace Core.Entities
     public class Product
     {
         public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
     }
 }
